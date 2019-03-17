132e68ffa33a211fc8495adb20d24eb1d8181c82
wolfachraf5 <47570819+wolfachraf5@users.noreply.github.com>
2019-03-17 22:33:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 
 
 const developers = ["527925159546454026"]
-const adminprefix = "$st";
+const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
