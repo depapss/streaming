23e74bcf6f9b400625d1845d832d398f8369f316
wolfachraf5 <47570819+wolfachraf5@users.noreply.github.com>
2019-03-28 19:16:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,19 +13,19 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.send(`**Status You   ${argresult}**`)
+      message.channel.send(`**تم فتح (اغلاق) لعب   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'wat')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Status You   ${argresult}**`)
+      message.channel.send(`**تم فتح (اغلاق) مشاهدة   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'lis')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Status You  ${argresult}**`)
+      message.channel.send(`**تم فتح(اغلاق)اغاني   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
-      message.channel.send(`**Status You ${argresult} **`)
+      message.channel.send(`**تم فتح (اغلاق) الستريمنق ${argresult} **`)
 }
 });
 
