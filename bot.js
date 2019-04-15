62cad805ade9a51573716ae861a8b6f3c971a0ce
wolfachraf5 <47570819+wolfachraf5@users.noreply.github.com>
2019-04-15 08:38:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ client.on('ready', () => {
 
 const developers = ["527925159546454026"]
 const adminprefix = "-";
+const hastebin = require('hastebin-gen');
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
@@ -41,6 +42,45 @@ if (message.content.startsWith(adminprefix + 'ava')) {
   if (message.content.startsWith(adminprefix + '2')) {
        message.channel.send(`**وعليكم السلام ورحمة الله تعال وبركاته  | ❤💖🌹 **`)
   }
+  
+client.on('message', message => {
+var PREFIX = '-';
+    if(message.content.startsWith(PREFIX + 'dis')) {
+            var args = message.content.split(' ').slice(1).join(' ');
+}
+      var array = [];
+      var i = 0;
+      if(args){
+client.users.filter(u => u.discriminator == args).map(u => {
+    if(i > 4){
+     return;
+    }
+    i = i + 1;
+ 
+   array.push(`${u.tag}`);
+});
+}
+hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
+    message.channel.send(`${l}`);
+}).catch(console.error);
+});
+client.on('message' , message => {
+var PREFIX = '-';
+if(message.content === `${PREFIX}dis         `         ) {
+                      let array = [];
+                      var i = 0;
+client.users.filter(u => u.discriminator == message.author.discriminator).map(u => {
+    if(i > 4){
+     return;
+    }
+    i = i + 1;
+   array.push(`${u.tag}`);
+});
+hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
+    message.channel.send(`${l}`);
+}).catch(console.error);
+ 
+        }
 });
 
 client.login(process.env.BOT_TOKEN);
