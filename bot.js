b2d4897643852dd3543462a3deea642cb343de0c
wolfachraf5 <47570819+wolfachraf5@users.noreply.github.com>
2019-05-15 21:48:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["541322693178425358"]
+const developers = ["527925159546454026"]
 const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
