58459c06f59a34ca333b57ead6dba7340c31d997
Mr.creater <47570819+wolfachraf5@users.noreply.github.com>
2019-08-27 19:15:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["527925159546454026"]
+const developers = ["589175566457110534"]
 const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
